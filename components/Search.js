417eac67637db34eed8557d2c8e77e7a3510162c
andrewgurung <andrewgurungg@gmail.com>
2017-04-09 18:04:16 -0400

    Search input text component

--- components/Search.js
+++ components/Search.js
@@ -3,9 +3,18 @@ const React = require('react');
 var Search = React.createClass({
   render() {
     return (
-      <div>
-        <h2>Search Component</h2>
-      </div>
+      <form id="geocoding_form" className="form-horizontal">
+        <div className="form-group">
+          <div className="col-xs-12 col-md-6 col-md-offset-3">
+            <div className="input-group">
+              <input type="text" className="form-control" id="address" placeholder="Find a location..."/>
+              <span className="input-group-btn">
+                <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </form>
     );
   }
 });
