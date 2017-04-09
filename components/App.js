cac1cce5fec9da2b18563fba4a2ac4298844aaf6
andrewgurung <andrewgurungg@gmail.com>
2017-04-09 10:40:31 -0400

    Replace react with react-dom for render + App module init

---
+++ components/App.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Your Google Maps Locations</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.getElementById('main').innerHTML = "TEST";
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('main')
+);
