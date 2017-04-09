7648d678c9bb9958725fb6330c8fae7bc509d5ed
andrewgurung <andrewgurungg@gmail.com>
2017-04-09 18:18:50 -0400

    Render current location

--- components/CurrentLocation.js
+++ components/CurrentLocation.js
@@ -2,9 +2,16 @@ const React = require('react');
 
 var CurrentLocation = React.createClass({
   render() {
+    var starClassName = "glyphicon glyphicon-star-empty";
+
+    if(this.props.favorite){
+			starClassName = "glyphicon glyphicon-star";
+		}
+
     return (
-      <div>
-        <h2>Current Location</h2>
+      <div className="current-location col-xs-12 col-md-6 col-md-offset-3">
+        <h4 id="save-location"></h4>
+        <span className={starClassName} aria-hidden="true"></span>
       </div>
     );
   }
