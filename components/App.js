f3b53d81c14392dad461737e59cd700de4214f2d
andrewgurung <andrewgurungg@gmail.com>
2017-04-09 21:26:41 -0400

    Render Gmaps with Washington DC as default

--- components/App.js
+++ components/App.js
@@ -5,12 +5,29 @@ var CurrentLocation = require('./CurrentLocation');
 var LocationList = require('./LocationList');
 
 var App = React.createClass({
+  getInitialState() {
+    // Extract favorites from local storage
+    var favorites = [];
+    if(localStorage.favorites) {
+      favorites = JSON.parse(localStorage.favorites);
+    }
+
+    // Set default address
+    return {
+      favorites: favorites,
+      currentAddress: 'Washington DC, USA',
+      mapCoordinates: {
+        lat: 38.9072,
+        lng: -77.0369
+      }
+    };
+  },
   render() {
     return (
       <div>
         <h1>Your Google Maps Locations</h1>
         <Search />
-        <Map />
+        <Map lat={this.state.mapCoordinates.lat} lng={this.state.mapCoordinates.lng}/>
         <CurrentLocation />
         <LocationList />
       </div>
--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,38 @@
 const React = require('react');
 
 var Map = React.createClass({
+  // React Component lifecycle method that is
+  // invoked immediately after a component is mounted.
+  componentDidMount() {
+    // This makes sure that our map initialization code is run the first time.
+    this.componentDidUpdate();
+  },
+
+  // Invoked immediately after updating occurs. This method is not called for the initial render.
+  componentDidUpdate() {
+    if(this.lastLat == this.props.lat && this.lastLng == this.props.lng){
+			// The map has already been initialized at this address.
+			// Return from this method so that we don't reinitialize it (and cause it to flicker).
+			return;
+		}
+
+    this.lastLat = this.props.lat;
+    this.lastLng = this.props.lng;
+
+    // Init Google maps
+    var map = new GMaps({
+      el: "#map",
+      lat: this.props.lat,
+      lng: this.props.lng
+    });
+
+    // Add Marker
+    map.addMarker({
+			lat: this.props.lat,
+			lng: this.props.lng
+		});
+  },
+
   render() {
     return (
       <div className="map-holder">
