9526004953f166207d7194cfa7db2e9f1e2942f8
andrewgurung <andrewgurungg@gmail.com>
2017-04-09 22:17:57 -0400

    Render Current Location

--- components/App.js
+++ components/App.js
@@ -22,13 +22,68 @@ var App = React.createClass({
       }
     };
   },
+
+  toggleFavorite(address) {
+    if(this.isAddressInFavorites) {
+      this.removeFromFavorites(address);
+    } else {
+      this.addToFavorites(address);
+    }
+  },
+
+  addToFavorites(address) {
+    var favorites = this.state.favorites;
+
+    favorites.push({
+      address: address,
+      date: Date.now()
+    });
+
+    this.setState({
+      favorites: favorites
+    });
+
+    this.localStorage.favorites = JSON.stringify(favorites);
+  },
+
+
+  removeFromFavorites(address){
+		var favorites = this.state.favorites;
+		var index = -1;
+
+		for(var i = 0; i < favorites.length; i++){
+			if(favorites[i].address == address){
+				index = i;
+				break;
+			}
+		}
+		// If it was found, remove it from the favorites array
+		if(index !== -1){
+			favorites.splice(index, 1);
+			this.setState({
+				favorites: favorites
+			});
+			localStorage.favorites = JSON.stringify(favorites);
+		}
+	},
+
+	isAddressInFavorites(address){
+		var favorites = this.state.favorites;
+		for(var i = 0; i < favorites.length; i++){
+			if(favorites[i].address == address){
+				return true;
+			}
+		}
+		return false;
+	},
+
   render() {
     return (
       <div>
         <h1>Your Google Maps Locations</h1>
         <Search />
         <Map lat={this.state.mapCoordinates.lat} lng={this.state.mapCoordinates.lng}/>
-        <CurrentLocation />
+        <CurrentLocation address={this.state.currentAddress} favorite={this.isAddressInFavorites(this.state.currentAddress)} onFavoriteToggle={this.toggleFavorite} />
         <LocationList />
       </div>
     );
--- components/CurrentLocation.js
+++ components/CurrentLocation.js
@@ -1,17 +1,22 @@
 const React = require('react');
 
 var CurrentLocation = React.createClass({
+  toggleFavorite() {
+    // Trigger onFavoriteToggle on App.js file
+    this.props.onFavoriteToggle(this.props.address);
+  },
+
   render() {
     var starClassName = "glyphicon glyphicon-star-empty";
 
     if(this.props.favorite) {
 			starClassName = "glyphicon glyphicon-star";
 		}
-    
+
     return (
       <div className="current-location col-xs-12 col-md-6 col-md-offset-3">
-        <h4 id="save-location"></h4>
-        <span className={starClassName} aria-hidden="true"></span>
+        <h4 id="save-location">{this.props.address}</h4>
+        <span className={starClassName} onClick={this.toggleFavorite} aria-hidden="true"></span>
       </div>
     );
   }
